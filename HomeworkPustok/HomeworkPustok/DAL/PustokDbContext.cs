cd00e4180bac82ed0dd065daac5b00bd7006af42
BehruzRehimli <bahruzkr@code.edu.az>
2023-06-11 17:55:56 +0400

    donePutokTask

---
+++ HomeworkPustok/HomeworkPustok/DAL/PustokDbContext.cs
@@ -0,0 +1,19 @@
+﻿using HomeworkPustok.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeworkPustok.DAL
+{
+    public class PustokDbContext:DbContext
+    {
+        public PustokDbContext(DbContextOptions<PustokDbContext>opt):base(opt)
+        {
+
+        }
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        public DbSet<BookImage> Bookİmages { get; set; }
+    }
+}
